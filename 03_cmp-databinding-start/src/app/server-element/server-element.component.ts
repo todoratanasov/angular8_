1134e7db5a0955b3c8155d0bd3fb432559d65a7e
todoratanasov <t.atanasov.atanasov@gmail.com>
2019-07-11 20:34:00 +0300

    upload with fourth app

--- 03_cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ 03_cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  OnDestroy
+} from "@angular/core";
 import { Content } from "@angular/compiler/src/render3/r3_ast";
 
 @Component({
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"]
-})
-export class ServerElementComponent implements OnInit {
+}) //когато искаме да ползваме различните lifecickle hooks трябва да ги добавим тук implements OnInit, OnChange ect.
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    OnDestroy {
   // този обект тук от този компонент с пропърти байндинг го подаваме в таг-а на компонента където се ползва. В този случай в html-а на app-component като, за да е видимо това пропърти от парент елемента в случая трябва да сложим @Input() отпред (Това се нарича декоратор като трябва да бъде импортнат горе) и попълвайки пропъртито element тук вече html-а на този компонент си се рендерира използвайки стойностите
   @Input() element: { type: string; name: string; content: string };
+  //конструктора винаги се изпълнява първи
   constructor() {}
-
+  //това се изпълнява второ и само то приема аргумент - този, който съдържа интересни неща като стойността сега, локалните променливи на елемента и предишната му стойност!!
+  ngOnChanges(changes: SimpleChanges) {}
+  //това се изпълнява трето
   ngOnInit() {}
+
+  //това се изпълнява при всяка промяна (цъкане на бутон и доста други)
+  ngDoCheck() {}
+  //този е следващия и се вика само и единствено веднъж
+  ngAfterContentInit() {}
+  //след всяка промяна се вика
+  ngAfterContentChecked() {}
+  //последен след като компонента бъде унищожен
+  ngOnDestroy() {}
 }
